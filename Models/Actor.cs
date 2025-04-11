ba4d893dfcf946a767c5962c8c41d3ca7134c2aa
mateo <atehortuamateo123@gmail.com>
2025-04-10 23:52:27 -0500

    funciona actor xd se pudo

--- Models/Actor.cs
+++ Models/Actor.cs
@@ -1,13 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-<<<<<<< HEAD
-using Microsoft.EntityFrameworkCore;
-using SistemaIndicadoresAPI.Data;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-=======
 using System.Text.Json.Serialization;
->>>>>>> be6b69498090f03faf1065325d2890ba9d838805
 
 public class Actor
 {
@@ -21,11 +14,5 @@ public class Actor
 
     [JsonIgnore]
     [ForeignKey("FkIdTipoActor")]
-<<<<<<< HEAD
-    public int FkIdTipoActor { get; set; } 
-
-    public required TipoActor? TipoActor { get; set; } = null!;
-=======
     public TipoActor? TipoActor { get; set; }
->>>>>>> be6b69498090f03faf1065325d2890ba9d838805
 }
